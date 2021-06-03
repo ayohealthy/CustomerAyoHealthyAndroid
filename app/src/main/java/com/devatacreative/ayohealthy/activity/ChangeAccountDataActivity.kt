659c1c66d9941f2ad05d10bc4eb04e767f8bb420
Shar AlNaja <firdausmaulana737@gmail.com>
2021-06-03 12:13:29 +0800

    Change data account layout

---
+++ app/src/main/java/com/devatacreative/ayohealthy/activity/ChangeAccountDataActivity.kt
@@ -0,0 +1,12 @@
+package com.devatacreative.ayohealthy.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devatacreative.ayohealthy.R
+
+class ChangeAccountDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_account_data)
+    }
+}
