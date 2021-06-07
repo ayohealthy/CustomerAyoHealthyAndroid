ad12fb681d3fa8e9bd5e995cffd21017d60f641e
Ariesta <odikk60@gmail.com>
2021-06-08 00:22:30 +0800

    Fixing profile image on home fragment

--- app/src/main/java/com/devatacreative/ayohealthy/activity/mainmenu/fragment/MainFragment.kt
+++ app/src/main/java/com/devatacreative/ayohealthy/activity/mainmenu/fragment/MainFragment.kt
@@ -1,11 +1,19 @@
 package com.devatacreative.ayohealthy.activity.mainmenu.fragment
 
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide
+import com.devatacreative.ayohealthy.R
 import com.devatacreative.ayohealthy.activity.adapter.HomeMenuAdapter
 import com.devatacreative.ayohealthy.databinding.FragmentMainBinding
 import com.devatacreative.ayohealthy.model.UserPrefModel
@@ -35,6 +43,15 @@ class MainFragment(private val userProfile: UserPrefModel) : Fragment() {
 
     private fun setViewData(){
         binding.userDisplayName.text = userProfile.name
+        Log.e("Profile URL : ", userProfile.avatarUrl.toString())
+        if (userProfile.avatarUrl != null){
+            context?.let {
+                Glide.with(it)
+                    .load(userProfile.avatarUrl)
+                    .error(R.drawable.blank_profile)
+                    .into(binding.homeProfileImage)
+            }
+        }
     }
 
     private fun setRecyclerView(){
@@ -43,7 +60,5 @@ class MainFragment(private val userProfile: UserPrefModel) : Fragment() {
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = GridLayoutManager(this.context, 2, GridLayoutManager.VERTICAL, false)
         recyclerView.adapter = adapter
-
     }
-
 }
