448957d88416acb9ed37ea0a9a9a690eb5dc3267
Ariesta <odikk60@gmail.com>
2021-06-07 18:13:30 +0800

    Fixing Bottom navigation view behavior

--- app/src/main/java/com/devatacreative/ayohealthy/activity/mainmenu/MainActivity.kt
+++ app/src/main/java/com/devatacreative/ayohealthy/activity/mainmenu/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.devatacreative.ayohealthy.R
+import com.devatacreative.ayohealthy.activity.mainmenu.fragment.AccountFragment
+import com.devatacreative.ayohealthy.activity.mainmenu.fragment.MainFragment
+import com.devatacreative.ayohealthy.activity.mainmenu.fragment.MainOrderFragment
 import com.devatacreative.ayohealthy.databinding.ActivityMainBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,68 +34,56 @@ class MainActivity : AppCompatActivity() {
         signInClient = GoogleSignIn.getClient(this, gso)
         val acc = GoogleSignIn.getLastSignedInAccount(this)
         if (acc != null){
-            binding.email.text = acc.email
-            binding.name.text = acc.displayName
+//            binding.email.text = acc.email
+//            binding.name.text = acc.displayName
         }
     }
 
     private fun initBottomLayout(){
 //        val pagerAdapter = SectionPagerAdapter(this)
 //        viewPager.adapter = pagerAdapter
-//        val bottomTab = binding.bottomNavigationView
-//        bottomTab.itemIconTintList = null
-//        bottomTab.setOnNavigationItemSelectedListener { item ->
-//            val fragment: Fragment?
-//            when (item.itemId) {
-//                R.id.menu_home -> {
-//                    item.title = "Home"
-//                    item.setIcon(R.drawable.home_active)
-//                    fragment = MainFragment()
-//                    getFragmentPage(fragment)
-////                    viewPager.currentItem = 0
+
+        val bottomTab = binding.bottomNavigationView
+        bottomTab.itemIconTintList = null
+        getFragmentPage(MainFragment())
+//        val navController = findNavController(R.id.nav_fragment)
+//        bottomTab.setupWithNavController(navController)
+
+        bottomTab.setOnNavigationItemSelectedListener { item ->
+            val fragment: Fragment?
+            when (item.itemId) {
+                R.id.menu_home -> {
+                    fragment = MainFragment()
+                    getFragmentPage(fragment)
+
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.menu_order -> {
+                    getFragmentPage(MainOrderFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.menu_profile -> {
+                    fragment = AccountFragment()
+                    getFragmentPage(fragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            true
+        }
+//        val navView: BottomNavigationView = binding.bottomNavigationView
 //
-//                    return@setOnNavigationItemSelectedListener true
-//                }
-//                R.id.menu_order -> {
-//                    item.setIcon(R.drawable.order_active)
-//                    getFragmentPage(MainOrderFragment())
-////                    viewPager.currentItem = 1
-//                }
-//                R.id.menu_profile -> {
-//                    item.setIcon(R.drawable.account_active)
-//                    fragment = AccountFragment()
-//                    getFragmentPage(fragment)
-////                    viewPager.currentItem = 2
-//                    return@setOnNavigationItemSelectedListener true
-//                }
-//            }
-//            false
-//        }
-        val navView: BottomNavigationView = binding.bottomNavigationView
-
-        val navController = findNavController(R.id.viewPager2)
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment, R.id.mainOrderFragment, R.id.accountFragment
-            )
-        )
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+//        val navController = findNavController(R.id.viewPager2)
+//
+//        navView.setupWithNavController(navController)
     }
 
-    private fun getFragmentPage(fragment: Fragment?): Boolean {
+    private fun getFragmentPage(fragment: Fragment?) {
         if (fragment != null) {
             supportFragmentManager
                 .beginTransaction()
-                .replace(R.id.viewPager2, fragment)
+                .replace(R.id.nav_fragment, fragment, fragment.javaClass.simpleName)
                 .commit()
-            return true
         }
-        return false
     }
 
     override fun onBackPressed() {
